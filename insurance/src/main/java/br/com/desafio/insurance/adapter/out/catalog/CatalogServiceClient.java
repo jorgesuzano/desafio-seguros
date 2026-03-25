1342638c14e8fb7f90ab508d088f59497a024eec
jorge <jorge_suzano@hotmail.com>
2026-03-25 20:41:26 -0300

    docs: adjustment variables

--- insurance/src/main/java/br/com/desafio/insurance/adapter/out/catalog/CatalogServiceClient.java
+++ insurance/src/main/java/br/com/desafio/insurance/adapter/out/catalog/CatalogServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "catalog-service", url = "http://localhost:8080/mock/catalog")
+@FeignClient(name = "catalog-service", url = "${catalog-service.url}")
 public interface CatalogServiceClient {
 
     @GetMapping("/products/{id}")
